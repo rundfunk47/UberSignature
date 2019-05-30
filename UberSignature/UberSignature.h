2cc09dff3528fe3d94741fac69e540c18ece38eb
Narek Mailian <narek.mailian@gmail.com>
2019-05-30 20:08:23 +0200

    Change project-structure, remove Pods

---
+++ UberSignature/UberSignature.h
@@ -0,0 +1,19 @@
+//
+//  UberSignature.h
+//  UberSignature
+//
+//  Created by Narek Mailian on 2019-05-30.
+//  Copyright © 2019 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UberSignature.
+FOUNDATION_EXPORT double UberSignatureVersionNumber;
+
+//! Project version string for UberSignature.
+FOUNDATION_EXPORT const unsigned char UberSignatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UberSignature/PublicHeader.h>
+
+
